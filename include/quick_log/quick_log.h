20697118790896199f16c71b8f60fa5dd69da4b0
SelvamKrishna <selvam.krishna0510@gmail.com>
2026-04-08 22:02:13 +0800

    feat: Styled logging feature implemented - listing, heading, bold, underline, highlight, styling macros

--- include/quick_log/quick_log.h
+++ include/quick_log/quick_log.h
@@ -11,12 +11,20 @@ extern "C" {
 // --- internal fn decl ---
 
 void q_log(int, const char* const, ...);
+
 void q_log_hr(void);
+void q_log_br(void);
 void q_log_head(const char* const, ...);
+void q_log_sub_head(const char* const, ...);
+void q_log_ul(const char* const, ...);
+void q_log_ol(size_t*, const char* const, ...);
+
 void q_log_test(bool, const char* const, ...);
 void q_log_assert(bool, const char*, int, const char* const, ...);
 
 // --- public API ---
+
+// leveled logging:
 
 #define qdbg(fmt, ...) \
     do { q_log(0, (fmt), ##__VA_ARGS__); } while (0)
@@ -30,11 +38,34 @@ void q_log_assert(bool, const char*, int, const char* const, ...);
 #define qerr(fmt, ...) \
     do { q_log(3, (fmt), ##__VA_ARGS__); } while (0)
 
-#define qhr() \
+#define qerr_ex(fmt, ...) \
+    do { q_log(3, (fmt), ##__VA_ARGS__); exit(EXIT_FAILURE); } while (0)
+
+// styled logging:
+
+#define qbold(text)      "\033[1m"  text "\033[0m"
+#define qunderline(text) "\033[4m"  text "\033[0m"
+#define qhighlight(text) "\033[47m" text "\033[0m"
+
+#define qlog_hr() \
     do { q_log_hr(); } while (0)
 
-#define qhead(text, ...) \
-    do { q_log_head((text), ##__VA_ARGS__); } while (0)
+#define qlog_br() \
+    do { q_log_br(); } while (0)
+
+#define qlog_head(fmt, ...) \
+    do { q_log_head((fmt), ##__VA_ARGS__); } while (0)
+
+#define qlog_subhead(fmt, ...) \
+    do { q_log_sub_head(fmt, ##__VA_ARGS__); } while (0)
+
+#define qlog_ul(fmt, ...) \
+    do { q_log_ul(fmt, ##__VA_ARGS__); } while (0)
+
+#define qlog_ol(num_ptr, fmt, ...) \
+    do { q_log_ol(num_ptr, fmt, ##__VA_ARGS__); } while (0)
+
+// testing and assertion:
 
 #define qtest(cnd) \
     do { q_log_test((cnd), #cnd); } while (0)
@@ -56,7 +87,7 @@ void q_log_assert(bool, const char*, int, const char* const, ...);
 #include <stdarg.h>
 #include <stdlib.h>
 
-#define _LOG_FMT_ARGS(fmt) \
+#define _LOG_FMT(fmt) \
     do {                     \
         va_list args;        \
         va_start(args, fmt); \
@@ -64,46 +95,68 @@ void q_log_assert(bool, const char*, int, const char* const, ...);
         va_end(args);        \
     } while (0)
 
-#define _ANSI_CODE(code) \
-    "\033[" #code "m"
+#define _ANSI(code) "\033[" #code "m"
+#define _ANSI_WRAP(code, text) _ANSI(code) text _ANSI(0)
 
 void q_log(int lvl, const char* const msg, ...)
 {
     static const char* LVL_LUT[] = {
-        _ANSI_CODE(36) "[DBUG]",
-        _ANSI_CODE(32) "[INFO]",
-        _ANSI_CODE(33) "[WARN]",
-        _ANSI_CODE(31) "[ERRO]"
+        _ANSI(36) "[DBUG]",
+        _ANSI(32) "[INFO]",
+        _ANSI(33) "[WARN]",
+        _ANSI(31) "[ERRO]"
     };
 
     if (lvl < 0 || lvl > 3) return;
 
-    printf("%s%s : ", LVL_LUT[lvl], _ANSI_CODE(0));
-    _LOG_FMT_ARGS(msg);
+    printf("%s%s : ", LVL_LUT[lvl], _ANSI(0));
+    _LOG_FMT(msg);
     printf("\n");
 }
 
-void q_log_hr(void) { printf("---\n"); }
+void q_log_hr(void) { printf(_ANSI_WRAP(2, "----------\n")); }
+void q_log_br(void) { printf(_ANSI(0)"\n"); }
 
-void q_log_head(const char* const heading, ...)
+void q_log_head(const char* const fmt, ...)
 {
     q_log_hr();
-    printf("%s", _ANSI_CODE(1));
-    _LOG_FMT_ARGS(heading);
-    printf("%s\n", _ANSI_CODE(0));
+    printf("%s", _ANSI(1));
+    _LOG_FMT(fmt);
+    printf("%s\n", _ANSI(0));
     q_log_hr();
+}
+
+void q_log_sub_head(const char* const fmt, ...)
+{
+    printf("\n%s", _ANSI(1));
+    _LOG_FMT(fmt);
+    printf("%s\n\n", _ANSI(0));
+}
+
+void q_log_ul(const char* const fmt, ...)
+{
+    printf(_ANSI_WRAP(2, "- "));
+    _LOG_FMT(fmt);
+    printf("\n");
+}
+
+void q_log_ol(size_t* number, const char* const fmt, ...)
+{
+    printf(_ANSI_WRAP(2, "%zu. "), number ? (*number)++ : 0);
+    _LOG_FMT(fmt);
+    printf("\n");
 }
 
 void q_log_test(bool cnd, const char* const msg, ...)
 {
     static const char* TEST_LUT[] = {
-        _ANSI_CODE(31) "[FAIL]",
-        _ANSI_CODE(32) "[PASS]",
+        _ANSI(31) "[FAIL]",
+        _ANSI(32) "[PASS]",
     };
 
-    printf("%s[TEST]%s", _ANSI_CODE(34), _ANSI_CODE(0));
-    printf("%s%s : ", TEST_LUT[cnd ? 1 : 0], _ANSI_CODE(0));
-    _LOG_FMT_ARGS(msg);
+    printf(_ANSI_WRAP(34, "[TEST]"));
+    printf("%s%s : ", TEST_LUT[cnd ? 1 : 0], _ANSI(0));
+    _LOG_FMT(msg);
     printf("\n");
 }
 
@@ -111,14 +164,15 @@ void q_log_assert(bool cnd, const char* file, int line, const char* const msg, .
 {
     if (cnd) return;
 
-    printf("%s[ASSERT]%s", _ANSI_CODE(41), _ANSI_CODE(0));
-    printf("%s[%s:%d]%s : ", _ANSI_CODE(1), file, line, _ANSI_CODE(0));
-    _LOG_FMT_ARGS(msg);
+    printf("%s[ASSERT]%s", _ANSI(41), _ANSI(0));
+    printf("%s[%s:%d]%s : ", _ANSI(2), file, line, _ANSI(0));
+    _LOG_FMT(msg);
     exit(EXIT_FAILURE);
 }
 
-#undef _LOG_FMT_ARGS
-#undef _ANSI_CODE
+#undef _LOG_FMT
+#undef _ANSI
+#undef _ANSI_WRAP
 
 #endif // QUICK_LOG_IMPL
 
--- tests/main.c
+++ tests/main.c
@@ -5,19 +5,34 @@ int main(void)
 {
     int a = 14;
 
-    qhead("Log Levels Showcase");
+    qlog_head("Log Levels Showcase");
     qdbg("Allocated (%zu) bytes at (%p)", sizeof(int), &a);
     qinfo("Initialized renderer");
     qwarn("Engine can be initialized only one");
     qerr("Process failed with code = %d", 404);
+    // qerr_ex("Will kill the program"); // Uncomment to test
 
-    qhead("Testing Showcase");
+    size_t line_n = 0;
+    qlog_head("Styles Showcase");
+    qlog_subhead("Sub Head 1");
+    qlog_ul("Point A");
+    qlog_ul("Point B");
+    qlog_ul("Point C");
+    qlog_subhead("Sub Head 2");
+    qlog_ol(&line_n, "Point A");
+    qlog_ol(&line_n, "Point B");
+    qlog_ol(&line_n, "Point C");
+    qlog_subhead("Sub Head 3");
+    printf("This is a "qbold("bold")" message.\n");
+    printf("This is a "qunderline("underlined")" message.\n");
+    printf("This is a "qhighlight("highlight")" message.\n");
+    qlog_br();
+
+    qlog_head("Testing / Assertion Showcase");
     qtest(a == 14);
     qtest(a != 14);
     qtest_s(false, "Failing test");
     qtest_s(true , "Passing test");
-
-    qhead("Assertion Showcase");
     qassert(a == 14);
     qassert_s(a != 14, "a should not be equal to 14");
 
